8e50229f11c65580eeed5a30d6f9f4f9efa76097
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-06-12 10:32:22 -0500

    loader added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
-  return <></>;
+  const { progress } = useProgress();
+  return (
+    <Html>
+      <span className="canvas-load">
+        <p
+          style={{
+            fontSize: 14,
+            color: "white",
+            fontWeight: 800,
+            marginTop: 40,
+          }}
+        >
+          {progress.toFixed(2)}%
+        </p>
+      </span>
+    </Html>
+  );
 };
 
 export default Loader;
