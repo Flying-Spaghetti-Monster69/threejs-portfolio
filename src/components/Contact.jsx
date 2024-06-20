431d396cc80ce1e853b8d3883a555a2a16bb4bcf
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-06-20 09:09:02 -0500

    Contact computer canvas and contact placeholder Added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../style";
+import { ComputerCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [text] = useTypewriter({
+    words: ["ideas?", "projects?", "goals?", "needs?", "dreams?"],
+    loop: false,
+  });
 
-export default Contact
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {};
+
+  const handleChange = (e) => {};
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-[#222222] p-8 rounded-2xl"
+      >
+        <h3 className={`${styles.sectionHeadText}`}>Do you have any</h3>
+        <span className={`${styles.sectionHeadText} text-primary`}>
+          {text}
+          <Cursor cursorColor="#08d108" />
+        </span>
+
+        <p className={`${styles.sectionSubText} text-primary`}>Get in touch!</p>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-[#161616] py-4 px-6 placeholder:text-gray-500 text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="font-medium mb-4">Your Email</span>
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-[#161616] py-4 px-6 placeholder:text-gray-500 text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="font-medium mb-4">Your message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-[#161616] py-4 px-6 placeholder:text-gray-500 text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            className="w-fit rounded-xl text-white shadow-md shadow-[#222222] py-3 px-8 outline-none bg-[#161616]"
+            type="submit"
+          >
+            {loading ? "sending..." : "send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <ComputerCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computer = () => {
-  return <div>Computer</div>;
+  const computer = useGLTF("./old_computer/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={5} />
+      <primitive object={computer.scene} scale={1} rotation={[0.1, 0, -0.1]} />
+    </mesh>
+  );
 };
 
-export default Computer;
+const ComputerCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          enablePan={false}
+          enableZoom={false}
+        />
+        <Computer />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
